ae8d6524278c26e14b48eb3b776984f352a22ab9
atari-monk <atari.monk1@gmail.com>
2024-11-30 21:04:00 +0100

    pip ver 0.1.1

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="atari_monk_cli_logger",
-    version="0.1.0",
+    name="atari-monk-cli-logger",
+    version="0.1.1",
     packages=find_packages(),
     entry_points={},
     install_requires=[],
